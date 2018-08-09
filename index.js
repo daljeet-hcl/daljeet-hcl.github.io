f1d356dc35d2c0c3689ded2e7e60d8e9c2f74a10
daljeet-hcl <daljeet.singh123@gmail.com>
2018-08-09 22:19:21 +0530

    fixed bug on apply settings

--- index.js
+++ index.js
@@ -26,7 +26,7 @@
 
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
   function closeNav() {
-      if (document.getElementById("mySidenav").style.width =="250") {
+      if (document.getElementById("mySidenav").style.width =="250px") {
           applySettings();
       }
       document.getElementById("mySidenav2").style.width = "0";
